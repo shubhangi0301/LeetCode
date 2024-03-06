83085d2d0391a278a814d50028d621d329922f03
Shubhangi Rastogi <70335325+shubhangi0301@users.noreply.github.com>
2024-03-06 23:46:15 +0530

    Time: 17 ms (56.71%) | Memory: 12 MB (47.44%) - LeetSync

---
+++ 14-longest-common-prefix/longest-common-prefix.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        prefix = strs[0]   #start = 'flower'
+        for string in strs[1:]:
+            while(string.find(prefix)!=0):
+                prefix = prefix[:-1]
+            if not prefix:
+                return ""
+        return prefix
+    
