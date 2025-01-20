c02041ad75f8bf09860ab59c899ec7b44180c5f9
Shubhangi Rastogi <70335325+shubhangi0301@users.noreply.github.com>
2025-01-20 20:29:11 +0530

    Time: 13 ms (90.70%) | Memory: 19.7 MB (22.07%) - LeetSync

--- 575-distribute-candies/distribute-candies.py
+++ 575-distribute-candies/distribute-candies.py
@@ -1,6 +1,6 @@
 class Solution:
     def distributeCandies(self, candyType: List[int]) -> int:
-        num_of_candies = int(len(candyType)/2)
+        num_of_candies = len(candyType)//2
         candy_set = set(candyType)
         if len(candy_set)<num_of_candies:
             return len(candy_set)
