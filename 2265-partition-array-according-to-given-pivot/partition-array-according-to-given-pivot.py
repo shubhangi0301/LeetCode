f34607158b3381986fc257ae28126ffab1407773
Shubhangi Rastogi <70335325+shubhangi0301@users.noreply.github.com>
2025-03-03 20:01:57 +0530

    Time: 43 ms (33.71%) | Memory: 34.5 MB (57.71%) - LeetSync

--- 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.py
+++ 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.py
@@ -1,26 +1,27 @@
 class Solution:
     def pivotArray(self, nums: List[int], pivot: int) -> List[int]:
-        # i = 0
-        # j = 0
-        # n = len(nums)
-        # new_arr = []
-        # for k in range(n):
-        #     if nums[k]<pivot:
-        #         new_arr.append(nums[k])
-        #         i+= 1
-        #     elif nums[k]>pivot:
-        #         new_arr.append(nums[k])
-        #         j+= 1
         n = len(nums)
-        new_arr = []
+        less_arr = []
+        grt_arr = []
+        eq_arr = []
         for i in range(n):
             if(nums[i]<pivot):
-                new_arr.append(nums[i])
-        for j in range(n):
-            if(nums[j]==pivot):
-                new_arr.append(nums[j])
-        for k in range(n):
-            if(nums[k]>pivot):
-                new_arr.append(nums[k])
-        return new_arr
+                less_arr.append(nums[i])
+            elif(nums[i]>pivot):
+                grt_arr.append(nums[i])
+            else:
+                eq_arr.append(nums[i])
+        less_arr.extend(eq_arr)
+        less_arr.extend(grt_arr)
+        return less_arr
+        # for i in range(n):
+        #     if(nums[i]<pivot):
+        #         new_arr.append(nums[i])
+        # for j in range(n):
+        #     if(nums[j]==pivot):
+        #         new_arr.append(nums[j])
+        # for k in range(n):
+        #     if(nums[k]>pivot):
+        #         new_arr.append(nums[k])
+        # return new_arr
 
