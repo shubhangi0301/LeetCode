2e2080fa199630ccf5f6c5414ad8277e3ae070cf
Shubhangi Rastogi <70335325+shubhangi0301@users.noreply.github.com>
2025-03-03 22:32:07 +0530

    Time: 22 ms (80.68%) | Memory: 19.3 MB (9.77%) - LeetSync

--- 594-longest-harmonious-subsequence/longest-harmonious-subsequence.py
+++ 594-longest-harmonious-subsequence/longest-harmonious-subsequence.py
@@ -4,9 +4,7 @@ class Solution:
         nums_count = Counter(nums)
         res = 0
         for x in nums_count:
-            if (x+1) in nums_count:
+            if x+1 in nums_count:
                 total = nums_count[x]+nums_count[x+1]
                 res = max(res,total)
-                # longest_sub  = nums_count[x]+nums_count[x+1]
-
         return res
