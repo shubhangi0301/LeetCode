3db3820a54b0cdd4a5211dce22b40a35bef759a9
Shubhangi Rastogi <70335325+shubhangi0301@users.noreply.github.com>
2025-03-06 20:51:58 +0530

    Time: 7 ms (72.80%) | Memory: 18 MB (97.30%) - LeetSync

--- 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.py
+++ 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.py
@@ -4,23 +4,24 @@ class Solution:
         output_arr = []
         freq = {}
         hash_dict = {}
-        # for i in range(n):
-        #     for j in range(n):
-        #         if grid[i][j] not in hash_dict:
-        #             hash_dict[grid[i][j]] = 1
-        #         else:
-        #             output_arr.append(grid[i][j])
-        # nums = hash_dict.keys()
-        # for k in range(1,n**2+1):
-        #     if k not in nums:
-        #         output_arr.append(k)
-        for row in grid:
-            for col in row:
-                freq[col] = freq.get(col,0)+1
+        for i in range(n):
+            for j in range(n):
+                if grid[i][j] not in hash_dict:
+                    hash_dict[grid[i][j]] = 1
+                else:
+                    output_arr.append(grid[i][j])
+        nums = hash_dict.keys()
+        for k in range(1,n*n+1):
+            if k not in nums:
+                output_arr.append(k)
+        return output_arr
+        # for row in grid:
+        #     for col in row:
+        #         freq[col] = freq.get(col,0)+1
 
-        for num in range(1,n*n+1):
-            if num not in freq:
-                missing = num
-            elif freq[num]==2:
-                repeat = num
-        return [repeat,missing]
+        # for num in range(1,n*n+1):
+        #     if num not in freq:
+        #         missing = num
+        #     elif freq[num]==2:
+        #         repeat = num
+        # return [repeat,missing]
